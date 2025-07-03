47a62c9882a6cb606da1ef63aca7336c94d69e9a
nathalie <nathalie@email.com>
2025-07-03 21:31:01 +1200

    he

--- technova-backend/index.js
+++ technova-backend/index.js
@@ -6,15 +6,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 app.get('/api/message', (req, res) => {
-    res.json( message: "Hello from TechNova's backend!ff" );
+    res.json({ message: "Hello from TechNova's backend!" });
 });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
