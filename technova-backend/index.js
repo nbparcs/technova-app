7d35cddc2e68b54e3290a2d432a128b05e42330f
nathalie <nathalie@email.com>
2025-07-04 12:57:06 +1200

    neww

--- technova-backend/index.js
+++ technova-backend/index.js
@@ -12,3 +12,11 @@ app.get('/api/message', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'build')));
+
+// For any unmatched route, send back React’s index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
